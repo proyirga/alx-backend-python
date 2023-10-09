e1d439962bf7b33e895f468374be9ee7333ea2d5
Yirga Mulaw <infoyirga12@gmail.com>
2023-10-09 12:28:54 +0300

    exec

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """Task wait random"""
 
 
@@ -8,6 +8,7 @@ from random import uniform
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Task wait random"""
     wait_random = __import__('0-basic_async_syntax').wait_random
     loop = asyncio.get_event_loop()
     return loop.create_task(wait_random(max_delay))
