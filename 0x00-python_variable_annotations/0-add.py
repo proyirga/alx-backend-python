9c88b3ed8113b22bd61c9daf3876a84711dd5492
Yirga Mulaw <infoyirga12@gmail.com>
2023-10-09 12:21:32 +0300

    Async

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+"""Basic Annotations - ADD"""
+
+
+def add(a: float, b: float) -> float:
+    """"ADD"""
+    return a + b
---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Multiple coroutines"""
+
+
+import asyncio
+from typing import List
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    # wait random
+    wait_random = __import__('0-basic_async_syntax').wait_random
+    coroutines = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*coroutines)
+    return sorted(delays)
