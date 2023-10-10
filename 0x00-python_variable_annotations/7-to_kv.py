8e84fe6e97907d732eb71e98fd0c451962fc6b86
Yirga Mulaw <infoyirga12@gmail.com>
2023-10-10 16:00:44 +0300

    TO KV

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""7-to_kv.py"""
+
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """TO_KV annotated function"""
+    return (k, v ** 2)
