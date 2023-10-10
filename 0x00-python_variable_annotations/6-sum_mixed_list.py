f082a39251ce884598033d3c510f2e86e4df2cd9
Yirga Mulaw <infoyirga12@gmail.com>
2023-10-10 17:28:09 +0300

    Mixed list

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,9 +2,9 @@
 """6-sum_mixed_list.py"""
 
 
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int, float]) -> float:
-    """SUM MIXED LIST"""
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """SUM MIXED LISvim 6-main.py"""
     return sum(mxd_lst)
