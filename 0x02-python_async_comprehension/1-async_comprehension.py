afee2e44969f28002da54e29daafe695f3d23262
Yirga Mulaw <infoyirga12@gmail.com>
2023-10-10 15:09:48 +0300

    Async generator comprehension

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""1-async_comprehension.py"""
+
+
+import asyncio
+import random
+
+
+async def async_generator():
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.randint(0, 10)
+
+async def async_comprehension():
+    return [i async for i in async_generator()]
