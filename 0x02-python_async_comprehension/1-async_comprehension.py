8a627a3c404e07c87e6ab9904482f9b2188787f6
Yirga Mulaw <infoyirga12@gmail.com>
2023-10-11 08:19:41 +0300

    Async Gen fix

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -6,7 +6,9 @@ import asyncio
 import random
 
 
-async_generator = __import__('async_generator')
+async_generator = __import__('async_generator').async_generator
+
 
 async def async_comprehension():
+    """ASYNC GENERATOR"""
     return [i async for i in async_generator()]
