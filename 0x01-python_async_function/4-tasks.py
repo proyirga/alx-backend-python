d83bc4295c9b5f6b2a10f7e09acd8b7a629ff868
proyirga <infoyirga12@gmail.com>
2023-12-11 13:30:24 +0300

    Wait random n

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,28 +1,18 @@
-#!/usr/bin/python3
-"""Wait N"""
-
+#!/usr/bin/env python3
+"""Multiple coroutines"""
 
 import asyncio
-import random
 from typing import List
 
-
-async def wait_random(max_delay: int = 10) -> float:
-    """wait random"""
+async def task_wait_random(max_delay: int) -> float:
+    """Asynchronous function that waits for a random amount of time."""
+    import random
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
 
-async def task_wait_random(max_delay: int) -> asyncio.Task:
-    """task wait random"""
-    loop = asyncio.get_event_loop()
-    return loop.create_task(wait_random(max_delay))
-
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """task wait n"""
-    delays = []
-    for _ in range(n):
-        task = await task_wait_random(max_delay)
-        delay = await task
-        delays.append(delay)
-    return delays
+    """Asynchronous function that waits for a random amount of time (task_wait_random)."""
+    coroutines = [task_wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*coroutines)
+    return sorted(delays)
