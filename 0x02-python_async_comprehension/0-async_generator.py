276425bccf58aea4bd02b5bdaf74d7cf3c14d1de
proyirga <infoyirga12@gmail.com>
2023-12-12 06:28:34 +0300

    PEP codestyle correction

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,11 +6,12 @@ import asyncio
 import random
 
 
-async def async_generator():
-    """
-    An async generator that yields a random number between 0
-    and 10 every second, 10 times.
-    """
-    for _ in range(10):
-        await asyncio.sleep(1)
-        yield random.uniform(0.0, 10.0)
+async def async_generator() -> AsyncGenerator[float, None]:
+        """
+        An async generator that yields a random number between 0.0 and 10.0 every second, 10 times.
+        Yields:
+           A random floating-point number between 0.0 and 10.0.
+        """
+        for _ in range(10):
+            await asyncio.sleep(1)
+            yield random.uniform(0.0, 10.0)
