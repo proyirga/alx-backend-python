e05153e8486ad90df2c4f878f43a7653a4cef711
proyirga <infoyirga12@gmail.com>
2023-12-12 06:42:23 +0300

    Pycodestyle

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,7 +7,9 @@ from typing import List
 
 
 async def task_wait_random(max_delay: int) -> float:
-    """Asynchronous function that waits for a random amount of time."""
+    """
+    Asynchronous function that waits for a random amount of time.
+    """
     import random
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
@@ -15,7 +17,10 @@ async def task_wait_random(max_delay: int) -> float:
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Asynchronous function that waits for a random amount of time (task_wait_random)."""
+    """
+    Asynchronous function that waits for a random amount of time
+    (task_wait_random).
+    """
     coroutines = [task_wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*coroutines)
     return sorted(delays)
