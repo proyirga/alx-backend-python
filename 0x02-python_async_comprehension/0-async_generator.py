90c979cb4720c1fa464564dfd7a10489d5cafb9f
proyirga <infoyirga12@gmail.com>
2023-12-12 06:06:28 +0300

    0. Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,7 +8,8 @@ import random
 
 async def async_generator():
   """
-  An async generator that yields a random number between 0 and 10 every second, 10 times.
+  An async generator that yields a random number between 0
+  and 10 every second, 10 times.
   """
   for _ in range(10):
     await asyncio.sleep(1)
